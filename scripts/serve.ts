b5955d5abf7ac98a42154b999e94c6bd6cbd5500
Fábio Maia <fabio.maia@itsector.pt>
2026-01-08 11:32:09 +0000

    feat: refined unused code

--- scripts/serve.ts
+++ scripts/serve.ts
@@ -1,9 +1,6 @@
 import path from "node:path";
 import express from "express";
 
-const args = process.argv.slice(2);
-const isMbcp = args.includes("--mbcp");
-
 const app = express();
 const PORT = 3000;
 
@@ -12,11 +9,6 @@ app.use("/cdn", express.static(path.join(__dirname, "../dist")));
 
 // Rota principal para servir o arquivo HTML
 app.get("/", (req, res) => {
-  if (isMbcp) {
-    res.sendFile(path.join(__dirname, "../public/index.mbcp.html"));
-    return;
-  }
-
   res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
